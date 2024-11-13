4955cf8ed581aa3f3be17992028f57cf8b4fb7a2
unknown <smjoshua09@gmail.com>
2024-11-13 03:45:37 +0300

    Initialize

---
+++ models/Mapping.go
@@ -0,0 +1,16 @@
+package models
+
+import "time"
+
+type Mapping struct {
+	CtmID       uint      `gorm:"column:ctm_id;primaryKey;autoIncrement"`
+	CatID       int64     `gorm:"column:cat_id;not null"`
+	ParentID    int64     `gorm:"column:parent_id;not null"`
+	DateCreated time.Time `json:"-" , gorm:"column:date_created;autoCreateTime"`
+	LastUpdate  time.Time `json:"-" , gorm:"column:last_update;autoUpdateTime"`
+	Contacts    []Contact `gorm:"foreignKey:CatID"`
+}
+
+func (Mapping) TableName() string {
+	return "tbl_category_mapping"
+}
